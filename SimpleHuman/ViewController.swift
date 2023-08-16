072aba03f8400009a01e8a01fef0cc4befa01e04
rig-0 <rigo@rig0.com>
2023-08-16 14:11:21 -0700

    Initial Commit

---
+++ SimpleHuman/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ©️ 2023 0100
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
