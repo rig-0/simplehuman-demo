30e07e9f2fdf59af6cc187a6255ba209dae86ab9
rig-0 <rigo@rig0.com>
2023-08-16 16:41:25 -0700

    Cleaned up colors

--- SimpleHuman/Source/Extensions/UIColor+AppColors.swift
+++ SimpleHuman/Source/Extensions/UIColor+AppColors.swift
@@ -6,7 +6,23 @@
 import UIKit
 
 extension UIColor {
+    
+    convenience init(rgb: (CGFloat, CGFloat, CGFloat), alpha: CGFloat = 1) {
+        self.init(red: rgb.0/255, green: rgb.1/255, blue: rgb.2/255, alpha: alpha)
+    }
+
+    static var random: UIColor {
+        return UIColor(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1.0
+        )
+    }
+}
+
+extension UIColor {
     struct App {
-        static var purple: UIColor { return UIColor(red: 177/255, green: 147/255, blue: 193/255, alpha: 1) }
+        static var purple: UIColor { return UIColor(rgb: (177, 147, 193)) }
     }
 }
--- SimpleHuman/Source/VIewControllers/DiscoverViewController.swift
+++ SimpleHuman/Source/VIewControllers/DiscoverViewController.swift
@@ -11,5 +11,41 @@ class DiscoverViewController: UIViewController {
         super.viewDidLoad()
         self.title = "discover"
         self.tabBarItem.title = ""
+        
+        let scrollView = UIScrollView()
+        self.view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        var lastView: UIView?
+        let numViews = 3
+        for i in 0 ..< numViews {
+            let pageView = UIView()
+            scrollView.addSubview(pageView)
+            pageView.translatesAutoresizingMaskIntoConstraints = false
+            pageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+            pageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+            pageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+            pageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+            
+            if let lastView = lastView {
+                pageView.topAnchor.constraint(equalTo: lastView.bottomAnchor).isActive = true
+            } else {
+                pageView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+            }
+            
+            if i == (numViews - 1) {
+                pageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+            }
+            
+            pageView.backgroundColor = .random
+            
+            lastView = pageView
+        }
     }
 }
+
+
